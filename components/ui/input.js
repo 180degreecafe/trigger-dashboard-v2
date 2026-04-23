e869c45c107f7a945ea2fe41aea57fcaae80f17d
180degreecafe <180.degre.bh@gmail.com>
2026-04-23 19:17:04 +0300

    Add Input component for user input fields

---
+++ components/ui/input.js
@@ -0,0 +1,8 @@
+export default function Input(props) {
+  return (
+    <input
+      className="w-full px-4 py-2 text-sm border border-gray-300 dark:border-gray-700 rounded-md bg-white dark:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      {...props}
+    />
+  );
+}
