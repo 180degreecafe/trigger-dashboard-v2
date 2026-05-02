fc716bc0b64b3f67e63fbdda389493744bbb50f2
180degreecafe <180.degre.bh@gmail.com>
2026-05-02 11:38:16 +0300

    Refactor DashboardLayout to include Sidebar

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,12 +1,23 @@
+import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
 
 export default function DashboardLayout({ children }) {
   return (
-    <>
-      <Header />
-      <div className="p-4 md:p-6 max-w-7xl mx-auto">
-        {children}
+    <div className="flex h-screen overflow-hidden">
+
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main */}
+      <div className="flex-1 flex flex-col bg-gray-50 dark:bg-gray-900">
+
+        <Header />
+
+        <main className="flex-1 overflow-y-auto p-4 md:p-6 max-w-7xl mx-auto w-full">
+          {children}
+        </main>
+
       </div>
-    </>
+    </div>
   );
 }
