d1f4a72305874196b9563c7914479eb175350404
180degreecafe <180.degre.bh@gmail.com>
2026-05-22 19:53:55 +0300

    Remove custom webpack rule for mp3 assets

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-  webpack(config) {
-
-    config.module.rules.push({
-
-      test: /\.(mp3)$/,
-
-      type: "asset/resource",
-    });
-
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
